91a59707e6e1619259f74cef36b72ba7dc349f17
jvphase <jvphase@ya.ru>
2022-03-25 23:26:42 +0300

    initial commit

---
+++ app/core/post/post.service.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('core.post').factory('Post', [
+  '$resource',
+  function ($resource) {
+    return $resource(
+      'posts/:postId.json',
+      {},
+      {
+        query: {
+          method: 'GET',
+          params: { postId: 'posts' },
+          isArray: true,
+        },
+      }
+    );
+  },
+]);
---
+++ app/post-list/post-list.component.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('postList').component('postList', {
+  templateUrl: 'post-list/post-list.template.html',
+  controller: [
+    'Post',
+    function PostListController(Post) {
+      this.posts = Post.query();
+    },
+  ],
+});
